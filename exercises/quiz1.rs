67ea4e54fc5d5ff3e31893c245a6a76ee8736a74
NikaJamburia <nikajamburia161@gmail.com>
2023-08-16 13:00:43 +0400

    Passed everything up to quiz1

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,11 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(quantity: i32) -> i32 {
+    let price_per_apple = if quantity > 40 { 1 } else { 2 };
+    price_per_apple * quantity
+}
 
 // Don't modify this function!
 #[test]
